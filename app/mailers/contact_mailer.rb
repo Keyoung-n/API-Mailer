87779adc124c1d45ee576c1376910a25c9215cd8
Keyoung-n <keang.nage.3@hotmail.com>
2018-10-29 20:25:51 +0200

    changed email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   default from: 'train.cool.78@gmail.com'
 
   def contact_email
-    mail(to: 'hullywould@gmail.com', subject: 'Your Requested mailer')
+    mail(to: 'hulleywould@gmail.com', subject: 'Your Requested mailer')
   end
 end
